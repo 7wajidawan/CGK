4b5773b2ee1d51ad71e7be8d09d709dbd9a475a2
7wajidawan <7wajidawan@gmail.com>
2026-06-06 05:15:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
-import os
 
-BOT_TOKEN = os.environ["8483160777:AAG872i0TMAxuAXcB6ZhqjcooPSoapo_y-U"]
-CHAT_ID = os.environ["534605127"]
+BOT_TOKEN = "8483160777:AAG872i0TMAxuAXcB6ZhqjcooPSoapo_y-U"
+CHAT_ID = "534605127"
 
 url = "https://api.coingecko.com/api/v3/coins/list/new"
 
@@ -16,7 +15,7 @@ telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 requests.post(
     telegram_url,
     data={
-        "chat_id": 534605127,
+        "chat_id": CHAT_ID,
         "text": "Bot is working"
     }
 )
