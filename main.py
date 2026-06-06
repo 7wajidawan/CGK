63872f56012a37325b899f0c5a07f1f1733f2cd9
7wajidawan <7wajidawan@gmail.com>
2026-06-06 05:55:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
+import os
 
-BOT_TOKEN = "8483160777:AAG872i0TMAxuAXcB6ZhqjcooPSoapo_y-U"
-CHAT_ID = "534605127"
+BOT_TOKEN = os.environ.get("8483160777:AAG872i0TMAxuAXcB6ZhqjcooPSoapo_y-U")
+CHAT_ID = os.environ.get("534605127")
 
 def send_telegram(message):
     telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -11,22 +12,17 @@ def send_telegram(message):
         "parse_mode": "HTML"
     })
 
-# Fetch new listings from CoinGecko
 url = "https://api.coingecko.com/api/v3/coins/list/new"
 response = requests.get(url)
-coins = response.json()  # Returns a LIST ✅
+coins = response.json()
 
 for coin in coins[:10]:
     message = (
         f"🆕 <b>New CGK Listing!</b>\n"
         f"🪙 Name: {coin['name']}\n"
         f"💎 Symbol: {coin['symbol'].upper()}\n"
-        f"🆔 ID: {coin['id']}\n"
-        f"⏰ Activated: {coin['activated_at']}"
+        f"🆔 ID: {coin['id']}"
     )
     send_telegram(message)
 
 print("Done")
-
-
-# Updated v2
